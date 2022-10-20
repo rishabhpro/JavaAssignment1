161fe6bdd0470c0e5b2da439aca23b93b05386cd
Anushka Srivastava <srivastavarishi29@gmail.com>
2022-10-20 18:48:11 +0530

    First Assignment

---
+++ Assignment_question2_code.java
@@ -0,0 +1,17 @@
+package java_assignment1;
+
+public class Assignment_question2_code {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 4;
+		for(int i = 1 ; i <= n ; i++) {
+			for(int j = 1 ; j <= n ; j++) {
+				System.out.print(i+" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
